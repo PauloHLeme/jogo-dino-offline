956f7ff573fda481a2f0fb1847a7a4330190f6f0
Paulo Henrique Leme <leme.pauloh@gmail.com>
2021-10-24 11:50:29 -0300

    projeto original finalizado

---
+++ script.js
@@ -0,0 +1,78 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector(".background");
+let position = 0;
+let isJumping = false;
+
+//Início interações do Dino
+
+//keycode.info é um site que nos mostra qual o código de uma tecla
+//A "barra de espaço" corresponde ao número 32
+function handleKeyUp(event){
+    if (event.keyCode === 32){
+        if(!isJumping){
+            jump();
+        }
+    }
+}
+
+function jump() {
+
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+            clearInterval(upInterval)
+
+            //descendo
+            let downInterval = setInterval(() =>{
+                if(position <= 0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }else{
+                    position -= 20;
+                    dino.style.bottom = position +'px'
+                }
+
+            },20)
+        }else{
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+
+    }, 20);
+}
+//Fim interações com Dino
+
+//Início interação Cactus
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    },20)
+
+    setTimeout(createCactus, randomTime)
+}
+
+createCactus();
+//Fim interação Cactus
+
+document.addEventListener('keydown',handleKeyUp);
